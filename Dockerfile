a0f1e8b4fd0f7dfd2d71003b34c514ca0aecc4e1
andrei ilief <andrei.ilief@gmail.com>
2020-02-18 09:52:25 +0200

    added skaffold configuration

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM golang
-
-WORKDIR /workspace
-
-# Assuming the source code is collocated to this Dockerfile, copy the whole
-# directory into the container that is building the Docker image.
 COPY . .
 RUN go get github.com/gorilla/mux
-RUN go build -o /myapp
+RUN CGO_ENABLED=0 go build -o /myapp
+RUN echo "nobody:x:65534:65534:Nobody:/:" > /etc_passwd
 
-# When a container is run from this image, run the binary
-CMD /myapp
+FROM scratch
+COPY --from=0 /myapp /myapp
+WORKDIR /workspace
+COPY --from=0 /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=0 /etc_passwd /etc/passwd
+# Assuming the source code is collocated to this Dockerfile, copy the whole
+# directory into the container that is building the Docker image.
+EXPOSE 8080
+USER nobody
--- main.go
+++ main.go
@@ -1,80 +1,27 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome home")
+	fmt.Fprintf(w, "welcome home %s!, with user %s and %s", getEnv("HOSTNAME", "default_hostname"), getEnv("DB_USER", "default_user"), getEnv("DB_URL", "default_url"))
 }
 
-type event struct {
-	ID          string `json:"ID`
-	Title       string `json:"Title`
-	Description string `json:Description`
-}
-
-type allEvents []event
-
-var events = allEvents{
-	{
-		ID:          "1",
-		Title:       "welcome to go",
-		Description: "this is the description",
-	},
-}
-
-func createEvent(w http.ResponseWriter, r *http.Request) {
-	var newEvent event
-	reqBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, "enter data with event and description only")
+func getEnv(key, fallback string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = fallback
 	}
-	json.Unmarshal(reqBody, &newEvent)
-	events = append(events, newEvent)
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newEvent)
-
+	return value
 }
-
-func getAllEvents(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(events)
-}
-
-func getOneEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for _, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			json.NewEncoder(w).Encode(singleEvent)
-		}
-	}
-}
-
-func deleteEvent(w http.ResponseWriter, r *http.Request) {
-	eventID := mux.Vars(r)["id"]
-
-	for i, singleEvent := range events {
-		if singleEvent.ID == eventID {
-			events = append(events[:i], events[i+1:]...)
-			fmt.Fprintf(w, "event %v has been deleted", eventID)
-		}
-	}
-}
-
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	router.HandleFunc("/event", createEvent).Methods("POST")
-	router.HandleFunc("/events", getAllEvents).Methods("GET")
-	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
-	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
-
 }
