b7bffa94a6d15f8cf218045eaee6a50a4483034a
andrei ilief <andrei.ilief@gmail.com>
2020-02-18 10:23:33 +0200

    add logging

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gorilla/mux"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/deny" {
+		http.NotFound(w, r)
+		return
+	}
 	fmt.Fprintf(w, "welcome home %s!, with user %s and %s", getEnv("HOSTNAME", "default_hostname"), getEnv("DB_USER", "default_user"), getEnv("DB_URL", "default_url"))
 }
 
@@ -20,8 +22,16 @@ func getEnv(key, fallback string) string {
 	}
 	return value
 }
+
+func wrapHandlerWithLogging(wrapperHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("--> %s %s", req.Method, req.URL.Path)
+		wrapperHandler.ServeHTTP(w, req)
+	})
+}
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	//	router := mux.NewRouter().StrictSlash(true)
+	rootHandler := wrapHandlerWithLogging(http.HandlerFunc(homeLink))
+	http.Handle("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
